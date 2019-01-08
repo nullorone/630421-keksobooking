92c4921770f0addf77b57211ac43d41f9c6f065d
nullorone <sy.v@mail.ru>
2019-01-08 19:16:09 +0300

    Реализует функционал фильтрации

--- js/pins.js
+++ js/pins.js
@@ -1,6 +1,7 @@
 'use strict';
 (function () {
   var ENTER_KEYCODE = 13;
+  // var MAX_ADS = 5;
 
   var mapPinMain = document.querySelector('.map__pin--main');
 
@@ -39,7 +40,6 @@
   var generateSimilarPins = function (adsArray) {
     var fragment = document.createDocumentFragment();
     for (var i = 0; i < adsArray.length; i++) {
-      // fragment.appendChild(creatingPin(adsArray[i]));
       if (adsArray[i].offer) {
         fragment.appendChild(creatingPin(adsArray[i]));
       }
@@ -47,122 +47,23 @@
     return fragment;
   };
 
-  var onTypeHousingChange = function (evt, data) {
-    removedPins();
-    if (evt.target.value !== 'any') {
-      var filterType = evt.target.value;
-      var newData = data.filter(function (item) {
-        return item.offer.type === filterType;
-      });
-      mapPins.appendChild(generateSimilarPins(newData));
-    } else {
-      mapPins.appendChild(generateSimilarPins(data));
-    }
-  };
-
-  var onPriceHousingChange = function (evt, data) {
-    removedPins();
-    if (evt.target.value !== 'any') {
-      var filterPrice = evt.target.value;
-      var newData = data.filter(function (item) {
-        var priceToOptionsPrices = {
-          'low': (item.offer.price < 10000),
-          'middle': (item.offer.price >= 10000 && item.offer.price <= 50000),
-          'high': (item.offer.price > 50000)
-        };
-        return priceToOptionsPrices[filterPrice];
-      });
-      mapPins.appendChild(generateSimilarPins(newData));
-    } else {
-      mapPins.appendChild(generateSimilarPins(data));
-    }
-  };
-
-  var onRoomsHousingChange = function (evt, data) {
-    removedPins();
-    if (evt.target.value !== 'any') {
-      var filterType = evt.target.value;
-      filterType = Number(filterType);
-      var newData = data.filter(function (item) {
-        return item.offer.rooms === filterType;
-      });
-      mapPins.appendChild(generateSimilarPins(newData));
-    } else {
-      mapPins.appendChild(generateSimilarPins(data));
-    }
-  };
-
-  var onGuestsHousingChange = function (evt, data) {
-    removedPins();
-    if (evt.target.value !== 'any') {
-      var filterType = evt.target.value;
-      filterType = Number(filterType);
-      var newData = data.filter(function (item) {
-        return item.offer.guests === filterType;
-      });
-      mapPins.appendChild(generateSimilarPins(newData));
-    } else {
-      mapPins.appendChild(generateSimilarPins(data));
-    }
-  };
-
-  var onFeaturesHousingChange = function (evt, data) {
-    removedPins();
-    if (evt.target.checked) {
-      var filterType = evt.target.value;
-      var newData = data.filter(function (item) {
-        var compare = false;
-        item.offer.features.forEach(function (element) {
-          if (element === filterType) {
-            compare = true;
-          }
-          return compare;
-        });
-        return compare;
-      });
-      mapPins.appendChild(generateSimilarPins(newData));
-    } else {
-      mapPins.appendChild(generateSimilarPins(data));
-    }
-  };
-  var typeHousing = document.querySelector('#housing-type');
-  var priceHousing = document.querySelector('#housing-price');
-  var roomsHousing = document.querySelector('#housing-rooms');
-  var guestsHousing = document.querySelector('#housing-guests');
-  var featuresHousing = document.querySelector('#housing-features');
   var formFilters = document.querySelector('.map__filters');
   var formFiltersItems = formFilters.querySelectorAll('select, input');
   var mapPins = document.querySelector('.map__pins');
 
   // Отрисовка пинов на карте
-  var showSimilarPins = function (data) {
-    formFilters.addEventListener('click', function () {
-      typeHousing.addEventListener('change', function (evt) {
-        onTypeHousingChange(evt, data);
-      });
+  var showSimilarPins = function (dataHousing) {
 
-      priceHousing.addEventListener('change', function (evt) {
-        onPriceHousingChange(evt, data);
-      });
-
-      roomsHousing.addEventListener('change', function (evt) {
-        onRoomsHousingChange(evt, data);
-      });
-
-      guestsHousing.addEventListener('change', function (evt) {
-        onGuestsHousingChange(evt, data);
-      });
-
-      featuresHousing.addEventListener('change', function (evt) {
-        onFeaturesHousingChange(evt, data);
-      });
+    formFilters.addEventListener('change', function () {
+      window.filters.updatePins(dataHousing);
     });
 
-    mapPins.appendChild(generateSimilarPins(data));
+    mapPins.appendChild(generateSimilarPins(dataHousing));
   };
 
   var getDataSuccess = function (data) {
-    showSimilarPins(data);
+    var dataHousing = data;
+    showSimilarPins(dataHousing);
     formFiltersItems.forEach(function (element) {
       element.disabled = 0;
     });
@@ -184,13 +85,8 @@
     }
   };
 
-  var removedPins = function () {
-    while (mapPinMain.nextElementSibling) {
-      mapPinMain.nextElementSibling.remove();
-    }
-  };
-
   window.pins = {
-    renderPins: renderPins
+    renderPins: renderPins,
+    generateSimilarPins: generateSimilarPins
   };
 })();
